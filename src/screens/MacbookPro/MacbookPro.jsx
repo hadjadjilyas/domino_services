c777ccfd137a3fae8c737967724d1744e4207a90
hadjadjilyas <ilyashadjadj08@gmail.com>
2024-05-31 13:33:46 +0200

    first commit

---
+++ src/screens/MacbookPro/MacbookPro.jsx
@@ -0,0 +1,156 @@
+import React from "react";
+import "./style.css";
+
+export const MacbookPro = () => {
+  return (
+    <div className="macbook-pro">
+      <div className="div">
+        <div className="overlap-group">
+          <div className="frame">
+            <div className="frame-2">
+              <div className="text-wrapper">À propos de nous</div>
+              <div className="text-wrapper-2">Nos services</div>
+              <div className="text-wrapper-2">Contact</div>
+            </div>
+          </div>
+          <img className="logodomino-blanc" alt="Logodomino blanc" src="/img/logodomino-blanc-2.png" />
+        </div>
+        <div className="overlap">
+          <div className="rectangle" />
+          <p className="domino-services-des">
+            <span className="span">
+              Domino Services : <br />
+            </span>
+            <span className="text-wrapper-3">
+              Des Services d&#39;Entretien <br />
+              qui Font la Différence !
+            </span>
+          </p>
+        </div>
+        <div className="text-wrapper-4">Nos valeurs</div>
+        <div className="text-wrapper-5">Nos services</div>
+        <div className="text-wrapper-6">Qualité</div>
+        <div className="text-wrapper-7">Nettoyage des locaux professionnels</div>
+        <div className="text-wrapper-8">Nettoyage des bureaux</div>
+        <div className="text-wrapper-9">Nettoyage des immeubles</div>
+        <div className="text-wrapper-10">Nettoyage industriel</div>
+        <p className="p">Nettoyage de débarras et evacuation des surplus</p>
+        <div className="text-wrapper-11">Fiabilité</div>
+        <div className="text-wrapper-12">Flexibilité</div>
+        <div className="text-wrapper-13">Qui sommes-nous?</div>
+        <div className="overlap-2">
+          <div className="rectangle-2" />
+          <img className="noun-reliability" alt="Noun reliability" src="/img/noun-reliability-5665477.svg" />
+        </div>
+        <div className="overlap-3">
+          <div className="rectangle-2" />
+          <img className="noun-flexible-work" alt="Noun flexible work" src="/img/noun-flexible-work-6627866.svg" />
+        </div>
+        <div className="overlap-4">
+          <div className="ellipse" />
+          <img className="adobestock" alt="Adobestock" src="/img/adobestock-217135828-1.png" />
+        </div>
+        <p className="optimisez-vos">
+          Optimisez vos espaces professionnels pour stimuler la productivité de votre équipe. Maintenez une atmosphère
+          impeccable dans vos locaux et corridors. En nous confiant l&#39;entretien de vos locaux, vous garantissez leur
+          état irréprochable, car votre satisfaction est notre priorité.
+        </p>
+        <p className="pour-domino-services">
+          Pour Domino Services, les bureaux et espaces communs présentent souvent des défis d&#39;entretien majeurs.
+          Nous offrons des solutions efficaces pour maintenir ces espaces en excellent état. Nos interventions sont
+          flexibles et s&#39;adaptent à vos horaires de bureau, sans perturber votre activité.
+        </p>
+        <p className="pour-l-entretien-de">
+          Pour l&#39;entretien de vos parties communes, intérieures et extérieures, Domino Services vous garantit une
+          prestation de qualité, adaptée à vos besoins et à vos préférences. Nous assurons le bon fonctionnement de
+          votre immeuble et veillons à votre sécurité.
+        </p>
+        <p className="la-poussi-re-peut">
+          La poussière peut parfois ralentir vos machines, entraînant des retards dans votre production. Notre équipe de
+          professionnels assure le nettoyage et la protection de vos chaînes de production, tout en respectant les
+          normes et les exigences de sécurité. L&#39;entretien, c&#39;est synonyme de productivité accrue.
+        </p>
+        <p className="text-wrapper-14">
+          Pour le nettoyage des débarras, Domino Services propose une gamme complète de services aux particuliers et aux
+          professionnels. Nous intervenons pour vider vos caves, greniers et chantiers, assurant un nettoyage complet et
+          un enlèvement efficace des encombrants.
+        </p>
+        <img className="img" alt="Adobestock" src="/img/adobestock-100429299-1.png" />
+        <img className="capture-dcran" alt="Capture dcran" src="/img/capture-d-cran-2024-05-27-222219-1.png" />
+        <div className="div-wrapper">
+          <div className="text-wrapper-15">@2023 Domino Services</div>
+        </div>
+        <div className="overlap-5">
+          <div className="rectangle-3" />
+          <div className="rectangle-4" />
+          <div className="overlap-6">
+            <p className="nom">
+              <span className="text-wrapper-16">Nom</span>
+              <span className="text-wrapper-17">*</span>
+            </p>
+            <p className="pr-nom">
+              <span className="text-wrapper-16">Prénom</span>
+              <span className="text-wrapper-17">*</span>
+            </p>
+          </div>
+          <div className="rectangle-5" />
+          <p className="e-mail">
+            <span className="text-wrapper-16">E-mail</span>
+            <span className="text-wrapper-17">*</span>
+          </p>
+          <div className="rectangle-6" />
+          <p className="confirmation-e-mail">
+            <span className="text-wrapper-16">Confirmation E-mail</span>
+            <span className="text-wrapper-17">*</span>
+          </p>
+          <div className="rectangle-7" />
+          <p className="num-ro-de-t-l-phone">
+            <span className="text-wrapper-16">Numéro de téléphone</span>
+            <span className="text-wrapper-17">*</span>
+          </p>
+          <div className="rectangle-8" />
+          <div className="text-wrapper-18">Votre message</div>
+          <div className="overlap-7">
+            <div className="rectangle-9" />
+            <div className="text-wrapper-19">Envoyer</div>
+          </div>
+          <div className="text-wrapper-20">Contactez-nous</div>
+          <div className="overlap-8">
+            <div className="text-wrapper-21">Contact</div>
+            <div className="text-wrapper-22">contact@dominoservices.fr</div>
+            <div className="text-wrapper-23">Recrutement</div>
+            <div className="text-wrapper-24">candidature@dominoservices.fr</div>
+            <p className="text-wrapper-25">8/10 Rue des Blés, Saint-Denis 93200</p>
+            <img className="capture-dcran-2" alt="Capture dcran" src="/img/capture-d-cran-2024-05-27-230417-1.png" />
+            <p className="text-wrapper-26">01 55 93 11 50</p>
+            <img className="noun-location" alt="Noun location" src="/img/noun-location-4832828-1.svg" />
+            <img className="vector" alt="Vector" src="/img/vector.svg" />
+            <img className="noun-phone" alt="Noun phone" src="/img/noun-phone-6856463-1.svg" />
+            <img className="noun-email" alt="Noun email" src="/img/noun-email-6793880-1.svg" />
+          </div>
+        </div>
+        <p className="cr-e-en-et-bas">
+          Créée en 2024 et basée à Saint-Denis, Domino Services est une entreprise spécialisée dans les services
+          d&#39;entretien et de nettoyage. Nous intervenons partout en Île-de-France pour offrir des solutions de
+          qualité, adaptées à vos besoins professionnels. Notre équipe dévouée et expérimentée s&#39;engage à garantir
+          la propreté et le bien-être de vos espaces, en respectant les plus hauts standards de qualité et de fiabilité.
+        </p>
+        <img className="line" alt="Line" src="/img/line-1.svg" />
+        <p className="nous-nous-engageons">
+          Nous nous engageons à fournir des services d&#39;entretien irréprochables,
+        </p>
+        <p className="text-wrapper-27">
+          La ponctualité et la constance sont au cœur de notre activité. Vous pouvez compter sur nous!
+        </p>
+        <p className="nous-adaptons-nos"> Nous adaptons nos services à vos contraintes et à vos horaires</p>
+        <img className="adobestock-2" alt="Adobestock" src="/img/adobestock-163434960-1.png" />
+        <img className="adobestock-3" alt="Adobestock" src="/img/adobestock-86198156-1.png" />
+        <div className="overlap-9">
+          <div className="rectangle-2" />
+          <img className="noun-quality" alt="Noun quality" src="/img/noun-quality-6587334-1.svg" />
+        </div>
+        <img className="adobestock-4" alt="Adobestock" src="/img/adobestock-815059516-1.png" />
+      </div>
+    </div>
+  );
+};
